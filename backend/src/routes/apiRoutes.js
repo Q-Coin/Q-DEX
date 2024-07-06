9d53a6ea24541f47eb2e5817ffca244ac7d3a51f
aagogue <164860332+aagogue@users.noreply.github.com>
2024-07-06 18:25:31 +0200

    preparation pour l'integration des datas sur les tokens

--- backend/src/routes/apiRoutes.js
+++ backend/src/routes/apiRoutes.js
@@ -3,7 +3,7 @@ const apiService = require('../services/apiService');
 const router = express.Router();
 const injParam = require('../middlewares/injParam');
 
-router.get('/:network/*', injParam, async (req, res) => {
+router.get('/:network/swap/*', injParam, async (req, res) => {
   const network = req.params.network;
   const endpoint = req.params[0] + (Object.keys(req.query).length ? '?' + new URLSearchParams(req.query).toString() : '');
 
@@ -14,5 +14,17 @@ router.get('/:network/*', injParam, async (req, res) => {
     res.status(error.response.status || 500).json({ error: error.message });
   }
 });
+
+router.get('/:network/explorer/*', async (req, res) => {
+  const network = req.params.network;
+  const endpoint = req.params[0] + (Object.keys(req.query).length ? '?' + new URLSearchParams(req.query).toString() : '');
+
+  try {
+    res.json({ error: 'Not implemented' });
+  } catch (error) {
+    res.status(error.response.status || 500).json({ error: error.message });
+  }
+});
+
 
 module.exports = router;
